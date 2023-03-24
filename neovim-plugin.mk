5e5039d1356d43255d32b2feb86924bd3609e723
notomo <notomo.motono@gmail.com>
2023-03-25 08:50:24 +0900

    Enable to modify neovim-plugin.mk variables

--- neovim-plugin.mk
+++ neovim-plugin.mk
@@ -1,15 +1,16 @@
 # usage: include {this_file}
 
-PLUGIN_NAME:=$(basename $(notdir $(abspath .)))
-SPEC_DIR:=./spec/lua/${PLUGIN_NAME}
+PLUGIN_NAME ?= $(basename $(notdir $(abspath .)))
+FULL_PLUGIN_NAME ?= ${PLUGIN_NAME}.nvim
+SPEC_DIR ?= ./spec/lua/${PLUGIN_NAME}
 
 test: FORCE deps
 	vusted --shuffle ${SPEC_DIR}
 
 doc: FORCE
-	rm -f ./doc/${PLUGIN_NAME}.nvim.txt ./README.md
+	rm -f ./doc/${FULL_PLUGIN_NAME}.txt ./README.md
 	PLUGIN_NAME=${PLUGIN_NAME} nvim --headless -i NONE -n +"lua dofile('${SPEC_DIR}/doc.lua')" +"quitall!"
-	cat ./doc/${PLUGIN_NAME}.nvim.txt ./README.md
+	cat ./doc/${FULL_PLUGIN_NAME}.txt ./README.md
 
 vendor: FORCE
 	nvim --headless -i NONE -n +"lua require('vendorlib').install('${PLUGIN_NAME}', '${SPEC_DIR}/vendorlib.lua')" +"quitall!"
